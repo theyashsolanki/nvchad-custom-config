c359ff0d09322d89df9dd8a9bc06de8514e84194
theyashsolanki <solankiyash556@gmail.com>
2023-07-12 23:20:11 +0530

    auto format on save

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -6,6 +6,8 @@ end
 
 local b = null_ls.builtins
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 local sources = {
 
   -- webdev stuff
@@ -25,6 +27,16 @@ local sources = {
 null_ls.setup {
   debug = true,
   sources = sources,
+  on_attach = function(client, bufnr)
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format { bufnr = bufnr }
+        end,
+      })
+    end
+  end,
 }
-
-
