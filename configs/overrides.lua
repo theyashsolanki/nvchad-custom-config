0733c034738b35cf0d4e88ef197e6db1b70e0b40
theyashsolanki <solankiyash556@gmail.com>
2023-09-18 19:55:08 +0530

    refactor

--- configs/overrides.lua
+++ configs/overrides.lua
@@ -146,4 +146,41 @@ M.cmp = {
   },
 }
 
+M.ufo = {
+  preview = {
+    mappings = {
+      scrollB = "<C-b>",
+      scrollF = "<C-f>",
+      scrollU = "<C-u>",
+      scrollD = "<C-d>",
+      -- ["zR"] = { function() require("ufo").openAllFolds() end, desc = "Open all folds" },
+      -- ["zM"] = { function() require("ufo").closeAllFolds() end, desc = "Close all folds" },
+      -- ["zr"] = { function() require("ufo").openFoldsExceptKinds() end, desc = "Fold less" },
+      -- ["zm"] = { function() require("ufo").closeFoldsWith() end, desc = "Fold more" },
+      -- ["<leader>zp"] = { function() require("ufo").peekFoldedLinesUnderCursor() end, desc = "Peek fold" },
+    },
+  },
+  provider_selector = function(_, filetype, buftype)
+    local function handleFallbackException(bufnr, err, providerName)
+      if type(err) == "string" and err:match "UfoFallbackException" then
+        return require("ufo").getFolds(bufnr, providerName)
+      else
+        return require("promise").reject(err)
+      end
+    end
+
+    return (filetype == "" or buftype == "nofile") and "indent" -- only use indent until a file is opened
+      or function(bufnr)
+        return require("ufo")
+          .getFolds(bufnr, "lsp")
+          :catch(function(err)
+            return handleFallbackException(bufnr, err, "treesitter")
+          end)
+          :catch(function(err)
+            return handleFallbackException(bufnr, err, "indent")
+          end)
+      end
+  end,
+}
+
 return M
--- plugins.lua
+++ plugins.lua
@@ -24,12 +24,6 @@ local plugins = {
         end,
         dependencies = { { "nvim-tree/nvim-web-devicons" } },
       },
-      -- {
-      --   "jose-elias-alvarez/typescript.nvim",
-      --   config = function()
-      --     require "custom.configs.lspconfig"
-      --   end,
-      -- },
     },
     config = function()
       require "plugins.configs.lspconfig"
@@ -100,42 +94,7 @@ local plugins = {
     "kevinhwang91/nvim-ufo",
     event = { "User AstroFile", "InsertEnter" },
     dependencies = { "kevinhwang91/promise-async" },
-    opts = {
-      preview = {
-        mappings = {
-          scrollB = "<C-b>",
-          scrollF = "<C-f>",
-          scrollU = "<C-u>",
-          scrollD = "<C-d>",
-          -- ["zR"] = { function() require("ufo").openAllFolds() end, desc = "Open all folds" },
-          -- ["zM"] = { function() require("ufo").closeAllFolds() end, desc = "Close all folds" },
-          -- ["zr"] = { function() require("ufo").openFoldsExceptKinds() end, desc = "Fold less" },
-          -- ["zm"] = { function() require("ufo").closeFoldsWith() end, desc = "Fold more" },
-          -- ["<leader>zp"] = { function() require("ufo").peekFoldedLinesUnderCursor() end, desc = "Peek fold" },
-        },
-      },
-      provider_selector = function(_, filetype, buftype)
-        local function handleFallbackException(bufnr, err, providerName)
-          if type(err) == "string" and err:match "UfoFallbackException" then
-            return require("ufo").getFolds(bufnr, providerName)
-          else
-            return require("promise").reject(err)
-          end
-        end
-
-        return (filetype == "" or buftype == "nofile") and "indent" -- only use indent until a file is opened
-          or function(bufnr)
-            return require("ufo")
-              .getFolds(bufnr, "lsp")
-              :catch(function(err)
-                return handleFallbackException(bufnr, err, "treesitter")
-              end)
-              :catch(function(err)
-                return handleFallbackException(bufnr, err, "indent")
-              end)
-          end
-      end,
-    },
+    opts = overrides.ufo,
   },
   {
     "nvim-telescope/telescope-ui-select.nvim",
@@ -200,61 +159,6 @@ local plugins = {
     "tpope/vim-obsession",
     lazy = false,
   },
-  -- {
-  --   "karb94/neoscroll.nvim",
-  --   lazy = false,
-  --   config = function()
-  --     require("neoscroll").setup {
-  --       -- All these keys will be mapped to their corresponding default scrolling animation
-  --       mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
-  --       hide_cursor = true,          -- Hide cursor while scrolling
-  --       stop_eof = true,             -- Stop at <EOF> when scrolling downwards
-  --       respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-  --       cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-  --       easing_function = nil,       -- Default easing function
-  --       pre_hook = nil,              -- Function to run before the scrolling animation starts
-  --       post_hook = nil,             -- Function to run after the scrolling animation ends
-  --       performance_mode = false,    -- Disable "Performance Mode" on all buffers.
-  --     }
-  --   end,
-  -- },
-  -- add debugger for javascript and typescript
-  -- {
-  --   "folke/todo-comments.nvim",
-  --   requires = "nvim-lua/plenary.nvim",
-  --   lazy = false,
-  --   config = function()
-  --     require("todo-comments").setup {
-  --       -- your configuration comes here
-  --       -- or leave it empty to use the default settings
-  --       -- refer to the configuration section below
-  --     }
-  --   end,
-  -- },
-
-  -- {
-  --   "shellRaining/hlchunk.nvim",
-  --   lazy = false,
-  --   config = function()
-  --     require("hlchunk").setup {
-  --
-  --       blank = {
-  --         enable = false,
-  --       }
-  --     }
-  --   end,
-  -- },
-
-  -- To make a plugin not be loaded
-  -- {
-  --   "NvChad/nvim-colorizer.lua",
-  --   enabled = false,
-  -- },
-
-  -- All NvChad plugins are lazy-loaded by default
-  -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
-  -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
 }
 
--- { "mg979/vim-visual-multi", branch = "master", lazy = false, config = function() end, },
 return plugins
