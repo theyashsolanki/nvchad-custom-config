57bf743c75937035d57f445bcea0067504acbb38
theyashsolanki <solankiyash556@gmail.com>
2023-09-18 19:58:31 +0530

    refactor

--- chadrc.lua
+++ chadrc.lua
@@ -5,14 +5,13 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "onedark",
-  theme_toggle = { "onedark", "onedark" },
+  theme = "solarized_dark",
+  theme_toggle = { "solarized_dark", "solarized_dark" },
   transparency = true,
   lsp_semantic_tokens = true,
   hl_override = highlights.override,
   hl_add = highlights.add,
   -- change cursor color
-  vim.cmd "hi Cursor guibg=#61afef guifg=#000000",
   vim.cmd [[highlight IndentBlanklineContextChar guifg=#9D7CD8 gui=nocombine]],
   vim.cmd "hi def IlluminatedWordText guibg=#2E3440 gui=NONE",
   vim.cmd "hi def IlluminatedWordWrite gui=NONE guibg=#2E3440",
